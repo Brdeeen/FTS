cb574ccba4c3906566c43336d93c103060f5c37f
Brdeeen <117855010+Brdeeen@users.noreply.github.com>
2022-11-29 11:09:08 -0700

    done

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-// JavaScript Code
+document.getElementById("Button1").addEventListener("click", SayPhrase1);
+
+function SayPhrase1() {
+    alert("Disguise");
+}
+
+document.getElementById("Button2").addEventListener("click", SayPhrase2);
+
+function SayPhrase2() {
+    alert("Dozen");
+}
+
+document.getElementById("Button3").addEventListener("click", SayPhrase3);
+function SayPhrase3() {
+    alert("Never");
+}
+
+document.getElementById("Button4").addEventListener("click", SayPhrase4);
+function SayPhrase4() {
+    alert("No gain");
+}
+document.getElementById("Button5").addEventListener("click", SayPhrase5);
+function SayPhrase5() {
+    alert("When your having fun");
+}
